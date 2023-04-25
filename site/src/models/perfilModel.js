43227be83475cd777f785e443e4fbd15c5e296d8
Rostichelli <leskalves.douglas@gmail.com>
2023-04-24 23:33:41 -0300

    Adicionando função de cadastro de funcionários part1

--- site/src/models/perfilModel.js
+++ site/src/models/perfilModel.js
@@ -25,19 +25,19 @@ function cadastrar(nome, email, senha, cpf, perfilAdministrador) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO perfil (nome, email, senha, cpf, perfilAdministrador) VALUES ('${nome}', '${email}', '${senha}', '${cpf}' , '${perfilAdministrador}');
+        INSERT INTO perfil (nome, email, senha, cpf, perfilAdministrador) VALUES ('${nome}', '${email}ss', '${senha}', '${cpf}' , '${perfilAdministrador}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
-function cadastrarFuncionario(nome, email, senha, cpf) {
-    console.log("Acessando perfil modal L \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf);
+function cadastrarFuncionario(nome, email, senha, cpf, perfilAdministrador) {
+    console.log("Acessando perfil modal L \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf, perfilAdministrador);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO perfil (nome, email, senha, cpf) VALUES ('${nome}', '${email}', '${senha}', '${cpf}', NULL) ;
+        INSERT INTO perfil (nome, email, senha, cpf, perfilAdministrador) VALUES ('${nome}', '${email}', '${senha}', '${cpf}', '${perfilAdministrador}') ;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
