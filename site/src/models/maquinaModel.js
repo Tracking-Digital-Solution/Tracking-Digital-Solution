bb9ee488a198fd4e20cdf28e9b829bed75b4534d
Gabriel Macedo Vilas Boas <gabriel.boas@sptech.school>
2023-05-19 16:38:03 -0300

    Tela de TI (INCOMPLETA)

--- site/src/models/maquinaModel.js
+++ site/src/models/maquinaModel.js
@@ -30,7 +30,7 @@ function deletarMaquina(idFunc) {
 function buscarDadosTi(fkPerfil) {
     // console.log("Acessando MaquinaCorporativa modal \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
-        SELECT * FROM MaquinaCorporativa join Perfil where fkPerfil = ${fkPerfil};
+        SELECT * FROM MaquinaCorporativa inner join Perfil ON perfilAdministrador = ${fkPerfil} where fkPerfil = ${fkPerfil};
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
